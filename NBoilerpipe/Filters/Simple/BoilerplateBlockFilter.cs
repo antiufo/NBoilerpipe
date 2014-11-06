0b4c899c8b7f938d9a7d38fce878b85fb7ba64c5
Andrea Martinelli <martinelli-andrea@outlook.com>
2014-11-06 14:40:50 +0100

    Formatted code

--- NBoilerpipe/Filters/Simple/BoilerplateBlockFilter.cs
+++ NBoilerpipe/Filters/Simple/BoilerplateBlockFilter.cs
@@ -11,39 +11,38 @@ using Sharpen;
 
 namespace NBoilerpipe.Filters.Simple
 {
-	/// <summary>
-	/// Removes
-	/// <see cref="NBoilerpipe.Document.TextBlock">NBoilerpipe.Document.TextBlock</see>
-	/// s which have explicitly been marked as "not content".
-	/// </summary>
-	/// <author>Christian Kohlsch√ºtter</author>
-	public sealed class BoilerplateBlockFilter : BoilerpipeFilter
-	{
-		public static readonly BoilerplateBlockFilter INSTANCE = new BoilerplateBlockFilter
-			();
+    /// <summary>
+    /// Removes
+    /// <see cref="NBoilerpipe.Document.TextBlock">NBoilerpipe.Document.TextBlock</see>
+    /// s which have explicitly been marked as "not content".
+    /// </summary>
+    /// <author>Christian Kohlsch√ºtter</author>
+    public sealed class BoilerplateBlockFilter : BoilerpipeFilter
+    {
+        public static readonly BoilerplateBlockFilter INSTANCE = new BoilerplateBlockFilter();
 
-		/// <summary>Returns the singleton instance for BoilerplateBlockFilter.</summary>
-		/// <remarks>Returns the singleton instance for BoilerplateBlockFilter.</remarks>
-		public static BoilerplateBlockFilter GetInstance()
-		{
-			return INSTANCE;
-		}
+        /// <summary>Returns the singleton instance for BoilerplateBlockFilter.</summary>
+        /// <remarks>Returns the singleton instance for BoilerplateBlockFilter.</remarks>
+        public static BoilerplateBlockFilter GetInstance()
+        {
+            return INSTANCE;
+        }
 
-		/// <exception cref="NBoilerpipe.BoilerpipeProcessingException"></exception>
-		public bool Process(TextDocument doc)
-		{
-			IList<TextBlock> textBlocks = doc.GetTextBlocks();
-			bool hasChanges = false;
-			for (Iterator<TextBlock> it = textBlocks.Iterator(); it.HasNext(); )
-			{
-				TextBlock tb = it.Next();
-				if (!tb.IsContent())
-				{
-					it.Remove();
-					hasChanges = true;
-				}
-			}
-			return hasChanges;
-		}
-	}
+        /// <exception cref="NBoilerpipe.BoilerpipeProcessingException"></exception>
+        public bool Process(TextDocument doc)
+        {
+            IList<TextBlock> textBlocks = doc.GetTextBlocks();
+            bool hasChanges = false;
+            for (Iterator<TextBlock> it = textBlocks.Iterator(); it.HasNext(); )
+            {
+                TextBlock tb = it.Next();
+                if (!tb.IsContent())
+                {
+                    it.Remove();
+                    hasChanges = true;
+                }
+            }
+            return hasChanges;
+        }
+    }
 }
